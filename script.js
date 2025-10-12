f7be8421033790a9882e72a457bd58f3452f7f5e
AxelS <72924802+SHOOTSTV@users.noreply.github.com>
2025-10-12 19:40:15 +0200

    feat: :sparkles: add multiple books to display from array

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 const myLibrary = [];
 
+const book2 = new Book(crypto.randomUUID(), "Book 2", "Author 2", 200);
+const book3 = new Book(crypto.randomUUID(), "Book 3", "Author 3", 300);
+const book4 = new Book(crypto.randomUUID(), "Book 4", "Author 4", 400);
+const book5 = new Book(crypto.randomUUID(), "Book 5", "Author 5", 500);
+addBookToLibrary(book2);
+addBookToLibrary(book3);
+addBookToLibrary(book4);
+addBookToLibrary(book5);
+console.log(myLibrary);
+
 function Book(id, name, author, pages) {
   this.id = id;
   this.name = name;
@@ -11,6 +21,19 @@ function addBookToLibrary(book) {
   myLibrary.push(book);
 }
 
-const book2 = new Book(crypto.randomUUID(), "Book 2", "Author 2", 200);
-addBookToLibrary(book2);
-console.log(myLibrary);
+function displayBooks() {
+  const bookContainer = document.querySelector(".book-container");
+  bookContainer.innerHTML = "";
+  myLibrary.forEach((book) => {
+    const bookElement = document.createElement("div");
+    bookElement.classList.add("book");
+    bookElement.innerHTML = `
+      <h2>${book.name}</h2>
+      <p>Author: ${book.author}</p>
+      <p>Pages: ${book.pages}</p>
+    `;
+    bookContainer.appendChild(bookElement);
+  });
+}
+
+displayBooks();
